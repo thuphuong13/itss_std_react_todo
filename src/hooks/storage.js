e2b300daf1a16c0390b0c5253a39ba0b1b6bb30f
Your Name <you@example.com>
2021-04-08 13:57:38 +0000

    fix save todo

--- src/hooks/storage.js
+++ src/hooks/storage.js
@@ -16,15 +16,24 @@ function useStorage() {
 　
 　/* 副作用を使う */
   useEffect(() => {
+    const data = window.localStorage.getItem(STORAGE_KEY);
     
+    if (!data) {
+      window.localStorage.setItem(STORAGE_KEY, JSON.stringify([]));
+    } else {
+      setItems(JSON.parse(data));
+    }
   }, []);
 
   const putItems = items => {
     
+     window.localStorage.setItem(STORAGE_KEY, JSON.stringify(items));
+    setItems  (items);
   };
 
   const clearItems = () => {
-    
+     window.localStorage.setItem(STORAGE_KEY, JSON.stringify([]));
+    setItems([]);
   };
 
   return [items, putItems, clearItems];
