d0386b70b86a8b1540073399d69c1be7f15989d3
EC2 Default User <ec2-user@ip-172-31-74-151.ec2.internal>
2021-05-19 10:00:21 +0000

    Use Firebase

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import React from 'react'
-
 /* スタイルシート */
 import './styles/main.css';
 
 /* コンポーネント */
 import Todo from './components/Todo';
+
+import firebase from 'firebase';
+
+ var firebaseConfig = {
+    apiKey: "AIzaSyCpDVm_ak6er8oTntYomE2e9pCfw5yVPDs",
+    authDomain: "fir-sample-40891.firebaseapp.com",
+    projectId: "fir-sample-40891",
+    storageBucket: "fir-sample-40891.appspot.com",
+    messagingSenderId: "244745233430",
+    appId: "1:244745233430:web:528358fc6a0275108ec137"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
 
 function App() {
   return (
